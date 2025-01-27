19520836c2d52944e93820c99a8f18dcacf1c1ed
Dai Joseph <arthurdai@Daide-MacBook-Pro.local>
2025-01-27 16:49:31 +0000

    Initial commit

--- app.py
+++ app.py
@@ -429,7 +429,7 @@ def create_rich_menu_1():
                 height=1686,
             ),
             selected=True,
-            name="圖文選單1",
+            name="圖文選單 1",
             chat_bar_text="點選主選單或輸入想詢問的事項",
             areas=areas
         )
@@ -438,7 +438,7 @@ def create_rich_menu_1():
             rich_menu_request=rich_menu_to_create
         ).rich_menu_id
 
-        with open('./static/richmenu-template-guidem-01-a.png', 'rb') as image:
+        with open('./static/richmenu-template-guidem-01.png', 'rb') as image:
             line_bot_blob_api.set_rich_menu_image(
                 rich_menu_id=rich_menu_id,
                 body=bytearray(image.read()),
@@ -529,7 +529,7 @@ def create_rich_menu_2():
                     "action": {
                         "type": "uri",
                         "label":"網路掛號",
-                        "text": "https://www2.ndmctsgh.edu.tw/newwebreg/Register/Doctors?pos=B&DeptCode=312&DeptGroup=4"
+                        "uri": "https://www2.ndmctsgh.edu.tw/newwebreg/Register/Doctors?pos=B&DeptCode=312&DeptGroup=4"
                     }
                 },
                 {
@@ -563,7 +563,7 @@ def create_rich_menu_2():
 
         line_bot_api.set_default_rich_menu(rich_menu_id)
 
-#create_rich_menu_2()
+create_rich_menu_2()
 
 client = DeepSeekClient()  # ✅ 提前初始化
 
